568811f6b94673cb987c1eac77e36dce8f86d074
yuchienchen <yuchienc@yahoo.com>
2024-07-27 17:18:42 -0500

    Initialize

---
+++ playground.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from openaikey import OPENAI_KEY
+
+# result = requests.get("https://wheretheiss.at/w/ajax/realtime")
+# print(result.text)
+
+
+openai_endpoint = "https://api.openai.com/v1/chat/completions"
+headers = {
+    "Content-Type":"application/json",
+    "Authorization":f"Bearer{OPENAI_KEY}"
+}
+
+data = {
+    "model":"gpt-4o",
+    "messages":[
+        {
+            "role":"systems",
+            "content":"Respond with some positive emojis"
+        }
+    ]
+}
+
+response = requests.post(openai_endpoint, headers=headers, json=data)
+
+print(response.text)
