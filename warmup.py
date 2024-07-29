c9c16d9e6944ed1ec814e3fc44ddb587d2f7f1dd
yuchienchen <yuchienc@yahoo.com>
2024-07-28 19:03:44 -0500

    go thru all the valid_keys to find the invalid_keys

--- warmup.py
+++ warmup.py
@@ -11,6 +11,11 @@ def main():
     valid_keys = list(scenes.keys())
     print(valid_keys)
 
+    for valid_key in valid_keys:
+        scene_dict = scenes[valid_key]
+        choices = scene_dict["choices"] 
+        print(choices)
+
 
 
 if __name__ == "__main__":
