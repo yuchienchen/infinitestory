026e9855a32759b841c611615176b47ce12ad8d0
yuchienchen <yuchienc@yahoo.com>
2024-07-29 18:34:52 -0500

    print starter scene

--- infinite_story.py
+++ infinite_story.py
@@ -12,10 +12,18 @@ from openaikey import OPENAI_KEY
 client = OpenAI(api_key=OPENAI_KEY)
 STORY_NAME = "original_small"
 
+def print_scene(scene_data):
+    text = scene_data["text"]
+    print(text)
+
+
 def main():
     print("infinite story")
     story_data = json.load(open(f"data/{STORY_NAME}.json"))
-    print(story_data)
+    # print(story_data)
+    starter_key = 'start'
+    starter_scene = story_data["scenes"][starter_key]
+    print_scene(starter_scene)
 
 
 
