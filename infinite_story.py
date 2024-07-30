13af94edf02aa6c6d6d3e336d39f01dbaeb6c9f7
yuchienchen <yuchienc@yahoo.com>
2024-07-29 19:16:11 -0500

    get a valid choice

--- infinite_story.py
+++ infinite_story.py
@@ -12,6 +12,25 @@ from openaikey import OPENAI_KEY
 client = OpenAI(api_key=OPENAI_KEY)
 STORY_NAME = "original_small"
 
+
+def get_valid_choice(scene_data):
+    print_scene(scene_data)
+    amt_choice = len(scene_data["choices"])
+
+    choice = input("What do you choose? ")
+
+    # if it is a number
+    # if it is above 0
+    # if it is equal to or below the length of choices
+    while not choice.isdigit() or int(choice) - 1 < 0 or int(choice) - 1 >= amt_choice:
+        choice = input("Please enter a valid choice: ")
+
+    valid_idx = int(choice) - 1
+    valid_scene = scene_data(["choices"][valid_idx])
+
+    return valid_scene
+
+
 def print_scene(scene_data):
     text = scene_data["text"]
     print(text)
@@ -28,7 +47,7 @@ def main():
     # print(story_data)
     starter_key = 'start'
     starter_scene = story_data["scenes"][starter_key]
-    print_scene(starter_scene)
+    get_valid_choice(starter_scene)
 
 
 
